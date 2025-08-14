97166a31455f948477ee4363a747c5c611eab2f8
Medidi Sai Ganesh <medidi.10823408@ltimindtree.com>
2025-08-14 16:03:13 +0530

    fixed social icons alignments

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -44,9 +44,70 @@ export default function LoginPage({ searchParams }: PageProps) {
     signIn("github", { callbackUrl: "/" });
   };
 
+  const getLoginForm = () => { return (
+    <form className="space-y-6" onSubmit={handleSubmit}>
+    <div>
+      <label
+        htmlFor="username"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        Username
+      </label>
+      <div className="mt-2">
+        <input
+          id="username"
+          name="username"
+          type="text"
+          autoComplete="off"
+          required
+          value={inputs.username || ""}
+          onChange={handleChange}
+          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+
+    <div>
+      <div className="flex items-center justify-between">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Password
+        </label>
+      </div>
+      <div className="mt-2">
+        <input
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="off"
+          required
+          value={inputs.password || ""}
+          onChange={handleChange}
+          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+      </div>
+    </div>
+
+    <div>
+      <button
+        type="submit"
+        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Sign in
+      </button>
+    </div>
+    {searchParams.error && (
+      <p className="text-red-600 text-center capitalize">Login failed.</p>
+    )}
+  </form>
+
+  )};
+
   const getSocialLogins = () => {
     return (
-      <div>
+      <div className="">
         <div className="p-2">
           <button
             onClick={signInWithGoogle}
@@ -84,65 +145,13 @@ export default function LoginPage({ searchParams }: PageProps) {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        {getSocialLogins()}
-        <br />
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="off"
-                required
-                value={inputs.username || ""}
-                onChange={handleChange}
-                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="off"
-                required
-                value={inputs.password || ""}
-                onChange={handleChange}
-                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Sign in
-            </button>
-          </div>
-          {searchParams.error && (
-            <p className="text-red-600 text-center capitalize">Login failed.</p>
-          )}
-        </form>
+        <div className="mb-5">
+          {getLoginForm()}
+        </div>
+        <hr/>
+        <div className="mt-3 flex items-center justify-center">
+          {getSocialLogins()}
+        </div>
       </div>
     </div>
   );
